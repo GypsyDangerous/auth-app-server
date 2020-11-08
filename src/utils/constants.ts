5536f70c5b2c5e798853cbd41490d21b4675062d
David Snyder <davidgraygs4@gmail.com>
2020-11-07 19:46:20 -0500

    added more utilities

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,9 @@
-export const upload_path = "./uploads/images"
+export const upload_path = "./uploads/images";
+
+export const saltRounds = 13;
+
+export const passwordMin = 8,
+	passwordMax = 64,
+	usernameMin = 3,
+	usernameMax = 16,
+	emailMin = 7;
