a146c1d5b7a174319eb5d7b9e8be1df50d61bc77
David Snyder <davidgraygs4@gmail.com>
2020-11-07 19:49:24 -0500

    converted local variables to imports

--- src/api/users.ts
+++ src/api/users.ts
@@ -2,28 +2,10 @@ import User from "../models/User.model";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { NextFunction, Request, Response, Router } from "express";
-const router = Router();
-// import uuidv1 from "uuid/v1"
-import fileUpload from "../middleware/file-upload";
 import { AuthRequest } from "../types/Request";
-const saltRounds = 13;
-
-const hasUniqueEmail = async (req: AuthRequest | Request, res: Response, next: NextFunction) => {
-	if (!req.body.email) {
-		return next();
-	}
-	try {
-		const user = await User.findOne({ email: req.body.email });
-		if (!user) {
-			next();
-		} else {
-			res.status(400).json({ code: 400, message: "A user with that email already exists" });
-		}
-	} catch (err) {
-		res.status(400).json("Error: " + err.message);
-	}
-};
-
+import {hasUniqueEmail} from "../middleware"
+import {saltRounds} from "../utils/constants"
+const router = Router();
 
 router.delete("/delete/:id", async (req, res, next) => {
 	try {
@@ -49,8 +31,8 @@ router.patch(
 	hasUniqueEmail,
 	async (req: AuthRequest, res: Response, next: NextFunction) => {
 		const user = await User.findOne({ uuid: req.userData?.userId });
-		if(!user){
-			throw new Error("Invalid User Id")
+		if (!user) {
+			throw new Error("Invalid User Id");
 		}
 		const { username, email, password, photo, bio, phone } = req.body;
 		try {
